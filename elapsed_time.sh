8ad9f15ed2671171b003a70a9c9abba135a3db0f
Brian Boognevatz <boognevatz@protonmail.com>
2026-02-05 15:10:50 +0100

    measure the actual wire speed

---
+++ elapsed_time.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+start=$(date +%s.%N)
+
+trap 'end=$(date +%s.%N); \
+      awk -v s="$start" -v e="$end" \
+      "BEGIN { printf \"Total time: %.3f ms\n\", (e - s) * 1000 }"; \
+      exit' INT
+
+curl -o stream5.raw -s -w "%{time_total}\n" http://172.16.1.1/stream 
+
+
